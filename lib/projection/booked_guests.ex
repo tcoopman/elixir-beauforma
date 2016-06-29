8b73e555d2f9106a0b7b358f61ce8f467efed83e
Thomas Coopman <thomas.coopman@gmail.com>
2016-06-29 23:47:36 +0200

    added some more infrastructure

--- lib/projection/booked_guests.ex
+++ lib/projection/booked_guests.ex
@@ -15,8 +15,19 @@ defmodule Beauforma.Projection.BookedGuests do
     defstruct [:appointmentId, :guestId, :dateTime]
   end
 
-  def project(%State{} = state, %GuestBookedAppointment{appointmentId: aId, guestId: gId, appointmentDateAndTime: dateTime}) do
+  def project(%State{} = state, events) do
+    events
+    |> Enum.reduce(state, &project_event/2)
+  end
+
+  def number_of_guests(%State{map: map}) do
+    map
+    |> Map.keys
+    |> Enum.count
+  end
+
+  defp project_event(%GuestBookedAppointment{appointmentId: aId, guestId: gId, appointmentDateAndTime: dateTime}, %State{} = state) do
     %State{state | map: Map.put(state.map, gId, %Appointment{appointmentId: aId, guestId: gId, dateTime: dateTime})}
   end
-  def project(%State{} = state, _), do: state
+  defp project_event(_, %State{} = state), do: state
 end
--- test/projection/booked_guests_test.exs
+++ test/projection/booked_guests_test.exs
@@ -1,21 +1,55 @@
 defmodule Beauforma.Projection.BookedGuestsTest do
   use ExUnit.Case
-  alias Beauforma.Events.{GuestBookedAppointment}
+  alias Beauforma.Events.{
+    GuestBookedAppointment,
+    GuestRescheduledAppointment
+  }
   alias Beauforma.Projection.BookedGuests
+  alias Beauforma.Projection.BookedGuests.{State, Appointment}
   doctest Beauforma
 
 
-  test "Simple event" do
-    event = %GuestBookedAppointment{
-      appointmentId: 1,
-      guestId: "Olivier",
-      appointmentDateAndTime: "20160626",
-      feelGoodPackageId: 1,
-      subsidiaryId: 1
-    }
-    
-    state = BookedGuests.project(%BookedGuests.State{}, event)
+  test "Projection" do
+    events = [
+      %GuestBookedAppointment{
+        appointmentId: 1,
+        guestId: "Olivier",
+        appointmentDateAndTime: "20160626",
+        feelGoodPackageId: 1,
+        subsidiaryId: 1
+      },
+      %GuestBookedAppointment{
+        appointmentId: 2,
+        guestId: "Olivier",
+        appointmentDateAndTime: "20160626",
+        feelGoodPackageId: 2,
+        subsidiaryId: 1
+      },
+      %GuestRescheduledAppointment{
+        appointmentId: 2,
+        guestId: "Olivier",
+        appointmentDateAndTime: "20160625",
+        feelGoodPackageId: 2,
+        subsidiaryId: 1
+      },
+      %GuestBookedAppointment{
+        appointmentId: 3,
+        guestId: "Rose",
+        appointmentDateAndTime: "20160501",
+        feelGoodPackageId: 2,
+        subsidiaryId: 1
+      },
+      %GuestBookedAppointment{
+        appointmentId: 4,
+        guestId: "Rose",
+        appointmentDateAndTime: "20160501",
+        feelGoodPackageId: 2,
+        subsidiaryId: 1
+      },
+    ]
 
-    assert Map.has_key?(state.map, "Olivier")
+    state = BookedGuests.project(%State{}, events)
+
+    assert BookedGuests.number_of_guests(state) == 2
   end
 end
